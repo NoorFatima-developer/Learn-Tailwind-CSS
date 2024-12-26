3ee7e7bbb90d192e8fdcdac5f096ce9473df099d
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-12-27 02:40:07 +0500

    code is added

--- README.md
+++
@@ -1,1 +0,0 @@
-# Learn-Tailwind-CSS 
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["*"], // Sirf src folder ke andar HTML aur JS files ko scan karega
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
